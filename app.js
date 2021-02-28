d73063ff9d7118450dfc6eb2332a6ae04e76a3e3
Max <max.09@outlook.com>
2021-02-28 09:57:40 +0100

    cleanup + comment code

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
- 
-   function openNetlifyWidget() {
-    netlifyIdentity.open();
-}
 
-function greetUser() {
-    let user = netlifyIdentity.currentUser()
-    if (user == null){
-        document.getElementById('greeting').innerHTML = "You're not logged in."
-    } else {
-        document.getElementById('greeting').innerHTML = "Welcome back, "+ user.user_metadata.full_name
-        
-    }
-}
-
-netlifyIdentity.on('init', user => {
-    //User is not logged in
-    if (user == null) {
-        console.log('No user logged in.')
-
-    }else {
-        console.log('Welcome back, '+user);
-        greetUser(user)
-    }
-});
-
-netlifyIdentity.on('login', user => {
-    console.log('Login succesfull for', user);
-    greetUser(user)
-    
-});
-
-netlifyIdentity.on('logout', user => {
-    console.log('Logout');
-    greetUser(user)
-})
-
+//////////////////
+//Event Listener
+//////////////////
 window.addEventListener('DOMContentLoaded', () => {
     console.log('dom content loaded.');
 
@@ -57,7 +24,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
+//////////////////
+//Authentication - Netlify Identity
+//////////////////
+netlifyIdentity.on('init', user => {
+    //User is not logged in
+    if (user == null) {
+        console.log('No user logged in.')
 
+    }else {
+        console.log('Welcome back, '+user);
+        greetUser(user)
+    }
+});
+
+netlifyIdentity.on('login', user => {
+    console.log('Login succesfull for', user);
+    greetUser(user)
+    
+});
+
+netlifyIdentity.on('logout', user => {
+    console.log('Logout');
+    greetUser(user)
+})
+ 
+function openNetlifyWidget() {
+    netlifyIdentity.open();
+}
+
+
+
+///////////////////
+//CALL TO BACKEND 
+///////////////////
 function getArticleLinkFromShareIntent(){
     let parsedUrl = new URL(window.location.toString());
     let articleTitle = parsedUrl.searchParams.get('title')
@@ -87,6 +87,19 @@ function sendRequestToAWS(linkArticle) {
     }
     console.log("Response :", xmlHttp.response)
     
+}
+
+
+
+
+function greetUser() {
+    let user = netlifyIdentity.currentUser()
+    if (user == null){
+        document.getElementById('greeting').innerHTML = "You're not logged in."
+    } else {
+        document.getElementById('greeting').innerHTML = "Welcome back, "+ user.user_metadata.full_name
+        
+    }
 }
 
 
